a7970e179bae495261deb0fc7ff4ae8ba724e257
aidancmatthews <aidancmatthews@gmail.com>
2022-09-29 11:05:55 -0400

    AltView Started
    -Rose Cube rotation (likely to change)
    -Alternate view (may refine for efficiency)
    -gimmick.js temporarily disabled for clean logs

--- threeproject/scripts/gimmicks.js
+++ threeproject/scripts/gimmicks.js
@@ -1,6 +1,6 @@
-let ageDisplay = document.getElementById("ageDisplay");
+// let ageDisplay = document.getElementById("ageDisplay");
 
-setInterval(() => {
-    const time = (new Date() - new Date(1026398100000)) / (1000 * 60 * 60 * 24 * 365.25);
-    ageDisplay.innerText = time.toString().substring(0, 12);
-}, 50)
+// setInterval(() => {
+//     const time = (new Date() - new Date(1026398100000)) / (1000 * 60 * 60 * 24 * 365.25);
+//     ageDisplay.innerText = time.toString().substring(0, 12);
+// }, 50)
